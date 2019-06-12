d2d4445a7505aa6f88d6a802479925ffdf574fee
Velibor Markovic <veljko85@users.noreply.github.com>
2019-06-12 18:57:33 +0200

    Add files via upload

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,18 @@
 var whiteBackground = document.getElementsByClassName("white-background");
 var van = document.getElementById("van");
 var freeDel = document.getElementById("free-del");
+var galHomeImg = document.getElementsByClassName("home-gal-img");
+var galHome = document.getElementById("galerija-home");
+var galHomeNas = document.getElementById("galerija-home-naslov");
 
 document.addEventListener('scroll', animate1);
 document.addEventListener('scroll', animate2);
 document.addEventListener('scroll', animate3);
+document.addEventListener('scroll', animate4);
 document.addEventListener('scroll', vanRide);
+document.addEventListener('scroll', galImgMove1);
+document.addEventListener('scroll', galImgMove2);
+document.addEventListener('scroll', galImgMove3);
 
 function inView(element) {
 
@@ -43,9 +50,35 @@ function animate3() {
 		}
 }
 
+function animate4() {
+	if (inView(whiteBackground[3])) {
+		whiteBackground[3].classList.add("white-background-animation");
+		whiteBackground[3].children[0].classList.add("main-content-animation");
+		}
+}
+
 function vanRide() {
 	if (inView(freeDel)) {
 		van.classList.add("van-ride");
+		}
+}
+
+function galImgMove1() {
+	if (inView(galHome)) {
+		galHomeImg[3].classList.add("img-in-side");
+		galHomeNas.style.opacity = "1";
+		}
+}
+
+function galImgMove2() {
+	if (inView(galHome)) {
+		galHomeImg[4].classList.add("img-in-side");
+		}
+}
+
+function galImgMove3() {
+	if (inView(galHome)) {
+		galHomeImg[5].classList.add("img-in-side");
 		}
 }
 
